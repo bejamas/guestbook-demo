eb55a1ad9f2ef117db544df9d08432f27fbb367a
Giridhar Talla <giridhar.talla2002@gmail.com>
2023-11-01 10:52:09 +0530

    add forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,179 @@
 import Image from 'next/image'
+// import { auth } from 'lib/auth'
+import { SignIn, SignOut } from './buttons'
+import { Suspense } from 'react'
+import Form from './form'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+					Giridhar&apos;s Guestbook
+				</p>
+			</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+			<div className='relative place-items-center z-[-1]'>
+				<h1 className='relative font-semibold dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-2xl mb-8 tracking-tighter'>
+					Sign my guestbook
+				</h1>
+				<Suspense>
+					<GuestbookForm />
+					<GuestbookEntries />
+				</Suspense>
+			</div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+			<div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
+		</main>
+	)
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+async function GuestbookForm() {
+	// const session = await auth()
+	const session = false
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	return session ? (
+		<>
+			<Form />
+			<SignOut />
+		</>
+	) : (
+		<SignIn />
+	)
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+async function GuestbookEntries() {
+	const entries = [
+		{
+			id: 1,
+			created_by: 'Giridhar',
+			body: 'Hey!',
+		},
+		{
+			id: 2,
+			created_by: 'Alice',
+			body: 'Sup?',
+		},
+		{
+			id: 3,
+			created_by: 'Bob',
+			body: "What's up?",
+		},
+		{
+			id: 4,
+			created_by: 'Carol',
+			body: "Not much, just bummin'",
+		},
+		{
+			id: 5,
+			created_by: 'David',
+			body: 'Same here...',
+		},
+		{
+			id: 6,
+			created_by: 'Eve',
+			body: "So, what's new?",
+		},
+		{
+			id: 7,
+			created_by: 'Frank',
+			body: 'Not much really. Just the usual.',
+		},
+		{
+			id: 8,
+			created_by: 'Grace',
+			body: 'Yeah, I feel you.',
+		},
+		{
+			id: 9,
+			created_by: 'Henry',
+			body: 'So, what are you up to today?',
+		},
+		{
+			id: 10,
+			created_by: 'Isabella',
+			body: "Not sure yet. Maybe I'll just chill out and watch some TV.",
+		},
+		{
+			id: 11,
+			created_by: 'Jack',
+			body: 'Sounds good. I might do the same.',
+		},
+		{
+			id: 12,
+			created_by: 'Kim',
+			body: 'So, what shows are you watching these days?',
+		},
+		{
+			id: 13,
+			created_by: 'Laura',
+			body: "I'm really into Stranger Things right now. Have you seen it?",
+		},
+		{
+			id: 14,
+			created_by: 'Michael',
+			body: "Yeah! It's so good! I'm eagerly waiting for the next season.",
+		},
+		{
+			id: 15,
+			created_by: 'Nicole',
+			body: 'Me too!',
+		},
+		{
+			id: 16,
+			created_by: 'Oliver',
+			body: 'So, what else are you up to?',
+		},
+		{
+			id: 17,
+			created_by: 'Patricia',
+			body: 'Not much else. Just trying to survive this heat wave.',
+		},
+		{
+			id: 18,
+			created_by: 'Quentin',
+			body: "Ugh, I know! It's been brutal.",
+		},
+		{
+			id: 19,
+			created_by: 'Rachel',
+			body: "I'm just hoping for a cold front to come through soon.",
+		},
+		{
+			id: 20,
+			created_by: 'Samuel',
+			body: 'Me too. My AC is starting to give up on me.',
+		},
+		{
+			id: 21,
+			created_by: 'Taylor',
+			body: "That sucks. I hope you don't have to go without AC for too long.",
+		},
+		{
+			id: 22,
+			created_by: 'William',
+			body: "Me too. I'm not sure how I would survive without it.",
+		},
+		{
+			id: 23,
+			created_by: 'Xander',
+			body: "Well, I guess I'll let you go. I have to go do some laundry.",
+		},
+		{
+			id: 24,
+			created_by: 'Yvonne',
+			body: "Same here. I've got a big pile of clothes to get through.",
+		},
+	]
+
+	return entries.map((entry) => (
+		<div key={entry.id} className='flex flex-col space-y-1 mb-4'>
+			<div className='w-full text-sm break-words'>
+				<span className='text-neutral-600 dark:text-neutral-400 mr-1'>
+					{entry.created_by}:
+				</span>
+				{entry.body}
+			</div>
+		</div>
+	))
 }
